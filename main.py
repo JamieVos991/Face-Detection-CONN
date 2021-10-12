d50e6f68b0c272579e0bd9f2de9c2dbfa631b372
Jamie <31694@ma-web.nl>
2021-10-12 22:45:35 +0200

    Python file

---
+++ main.py
@@ -0,0 +1,39 @@
+import cv2
+import time 
+import datetime 
+from time import sleep
+
+a = 0
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_fullbody_default.xml")
+
+while True:
+    _, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    if len(faces) + len(bodies) > 0:
+
+        a = a + 1
+        time.sleep(1)
+        print(a)
+
+        if a == 10: 
+            print("einde")
+
+    # for (x, y, width, height) in faces:
+    #    cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+
+    cv2.imshow("Camera", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
